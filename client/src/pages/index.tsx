5f0b273a956e8bf07a34e7816224687ecafd3f6c
Briam Bejarano <briambejarano006@gmail.com>
2023-06-01 18:57:10 -0500

    feat(components/cardHome):add component productCardHome and styles

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import ProductListing from "@/components/product-listing";
+import data from "../data.json"
 import Link from "next/link";
+import ProductInterface from "@/interfaces/productsInterface";
 
 export async function getStaticProps() {
   return {
@@ -10,5 +12,19 @@ export async function getStaticProps() {
 }
 
 export default function Home() {
-  return <div></div>
+
+  let dataProducts = data as ProductInterface[];
+  let arrayProducts: ProductInterface[] = []
+  let i: number = 0
+  while(dataProducts.length > i && arrayProducts.length < 8){
+    arrayProducts.push(dataProducts[i])
+    i = i+5
+  }
+
+  return (<div>
+    <Link href="/products">
+      <button>Ver todos los productos</button>
+    </Link>
+    <ProductListing arrayProducts={arrayProducts}/>
+  </div>)
 }
