616fd4cebe156964d683322f45146afb0aecba46
santiagoDeMarco <corvalansantiago29@gmail.com>
2023-06-02 12:31:15 -0300

    pain

--- server/src/modules/products/products.module.ts
+++ server/src/modules/products/products.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { productsProviders } from './products.providers';
