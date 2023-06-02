bb24c99704883a0263eb3fbfd4044c8515176541
Briam Bejarano <briambejarano006@gmail.com>
2023-06-02 00:05:35 -0500

    feat: repositioned button

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import ProductListingHome from "@/components/product-listing-home"; 
+import ProductListingHome from "@/components/product-listing-home";
 import data from "../data.json"
 import Link from "next/link";
 import ProductInterface from "@/interfaces/productsInterface";
@@ -25,15 +25,12 @@ export default function Home() {
   let dataProducts = data as ProductInterface[];
   let arrayProducts: ProductInterface[] = []
   let i: number = 0
-  while(dataProducts.length > i && arrayProducts.length < 9){
+  while (dataProducts.length > i && arrayProducts.length < 9) {
     arrayProducts.push(dataProducts[i])
-    i = i+5
+    i = i + 5
   }
 
   return (<div className="flex flex-col items-center ">
-    <Link href="/products">
-      <button className="bg-violet-900 rounded-xl	py-1 px-2 text-white hover:bg-violet-700 duration-300">Ver todos los productos</button>
-    </Link>
     <div className=" w-[1000px] h-[400] mx-auto my-0 ">
       <Carrousel loop >
         {images.map((src, i) => {
@@ -45,6 +42,9 @@ export default function Home() {
         })}
       </Carrousel>
     </div>
-    <ProductListingHome arrayProducts={arrayProducts}/>
+    <Link href="/products">
+      <button className="bg-violet-900 rounded-xl	py-1 px-2 mt-5 text-white hover:bg-violet-700 duration-300">Ver todos los productos</button>
+    </Link>
+    <ProductListingHome arrayProducts={arrayProducts} />
   </div>)
 }
