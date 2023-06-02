d4cdf0fa226bed16f7e289a92984e9643f8cfc57
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-02 00:18:58 -0400

    feat: redux ok

--- client/src/pages/favorites.tsx
+++ client/src/pages/favorites.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { NextPage } from "next";
+import { useTypedSelector } from "@/store/useTypeSelector";
+import { useDispatch } from "react-redux";
+import { getComments } from "@/store/actionCreators/getComments";
 
 export async function getStaticProps() {
   return {
@@ -10,7 +13,29 @@ export async function getStaticProps() {
 }
 
 const Favorites: NextPage = () => {
-  return <div>this is favorites page</div>;
+  const { comments, loading, error } = useTypedSelector(
+    (state) => state.comments
+  );
+  const dispatch = useDispatch();
+
+  const handleClick = async () => {
+    // @ts-ignore
+    await dispatch(getComments("1"));
+  };
+  return (
+    <div>
+      this is favorites page
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {JSON.stringify(comments)}
+      <button
+        className="bg-amber-300 border-2 border-black"
+        onClick={() => handleClick()}
+      >
+        test dispatch
+      </button>
+    </div>
+  );
 };
 
 export default Favorites;
