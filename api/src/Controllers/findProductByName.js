f23b248d74a3dfbc591e76e1492f355a60502bf3
Briam Bejarano <briambejarano006@gmail.com>
2023-06-06 18:16:39 -0500

    feat: create backend and routes

---
+++ api/src/Controllers/findProductByName.js
@@ -0,0 +1,16 @@
+const { Products } = require("../db")
+const { Op } = require("sequelize")
+
+const findProductByName = async (name) => {
+    const productByName = await Products.findAll({
+        where: {
+            name: {
+                [Op.iLike]: `%${name}%`,
+            }
+        }
+    })
+    return productByName;
+}
+
+module.exports = findProductByName;
+
