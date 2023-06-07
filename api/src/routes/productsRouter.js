7b1e28e6b2e455d7ee102ab8446c4afbb6c270a9
valentin urcullu <urculluvalentin@gmail.com>
2023-06-07 20:50:32 -0300

    paginado de seba

--- api/src/routes/productsRouter.js
+++ api/src/routes/productsRouter.js
@@ -8,20 +8,36 @@ const productsRouter = express.Router()
 
 productsRouter.get("/", async (req, res) => {
     try {
-        const {name, page, size} = req.query
+        const {name} = req.query
         if(name) {
             const productByName = await findProductByName(name)
            return res.status(200).json(productByName)
         }
+        const pageAsNumber = Number.parseInt(req.query.page)
+        const sizeAsNumber = Number.parseInt(req.query.size)
+        let page = 0;
+        if(!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+            page= pageAsNumber
+        }
+
+        let size = 11
+        if(!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 10) {
+            size = sizeAsNumber
+        }
+
         const allProducts = await Products.findAndCountAll({
         limit: size,
         offset: page * size
         })
-        res.status(200).json(allProducts)
+        res.status(200).json({
+            content: allProducts.rows,
+            totalPages: Math.ceil(allProducts.count / size)
+        })
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 })
+
 productsRouter.get("/:idProduct", async (req, res) => {
     try {
         const {idProduct} = req.params;
@@ -31,5 +47,6 @@ productsRouter.get("/:idProduct", async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 })
+
 
 module.exports = productsRouter;
