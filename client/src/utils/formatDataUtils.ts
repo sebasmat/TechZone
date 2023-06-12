a02af8574837e2c6019fa3a57a278c6260cd7f3a
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-12 02:00:33 -0400

    feat: shoppingCart ok all functionality

---
+++ client/src/utils/formatDataUtils.ts
@@ -0,0 +1,30 @@
+import ShoppingCartInterface from "@/interfaces/shoppingCartInterface";
+
+export const formatDataForLocal = (data: any): ShoppingCartInterface => {
+  return {
+    user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      profileIMG: data.profileIMG,
+      direction: data.direction,
+      cellPhone: data.cellPhone,
+      Gender: data.Gender,
+    },
+    products: data.Products.map((item: any) => {
+      return {
+        id: item.id,
+        name: item.name,
+        brand: item.brand,
+        category: item.category,
+        images: item.images,
+        description: item.description,
+        price: item.price,
+        avalaible: item.avalaible,
+        stock: item.stock,
+        quantity: item.Cart.quantity,
+      };
+    }),
+  };
+};
