70c43209815500442be3378a203ed698170fe8d5
BriamB02 <briambejarano006@gmail.com>
2023-06-14 17:37:56 -0500

    feat: Search Reducer error on product page resolved

--- client/src/store/reducers/combine.ts
+++ client/src/store/reducers/combine.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import searchReducer from "./searchReducer";
 import productsReducer from "@/store/reducers/productsReducer";
 import productReducer from "./index";
 import userReducer from "./userReducer";
@@ -7,7 +6,6 @@ import cartReducer from "./cartReducer";
 
 const reducers = combineReducers({
   product: productReducer,
-  searchs: searchReducer,
   products: productsReducer,
   user: userReducer,
   cart: cartReducer,
