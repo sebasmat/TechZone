446b52ff1a20fc87c13946bfa147459c4454b01b
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-14 23:24:02 -0400

    feat: testing daisyui for tailwind, dashboard with products and users and prepare for merge into developer branch

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Techzone`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/techzone`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -37,12 +37,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Products, Users , Favorites} = sequelize.models;
+const { Products, Users, Favorites } = sequelize.models;
 
 // Aca vendrian las relaciones
 
-Users.belongsToMany(Products, { through: Favorites});
-Products.belongsToMany(Users, { through: Favorites});
+Users.belongsToMany(Products, { through: Favorites });
+Products.belongsToMany(Users, { through: Favorites });
 
 const Cart = sequelize.define(
   "Cart",
