98d18da7c5961f29bb37cdfeeab642414986e4b0
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-19 01:27:19 -0400

    feat: sales admin option and fix sales error key duplicate

--- client/src/pages/admin/sales.tsx
+++ client/src/pages/admin/sales.tsx
@@ -1,16 +1,16 @@
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { NextPageWithLayout } from "@/pages/_app";
 import AdminLayout from "@/layout/admin-layout";
-import Products from "@/pages/admin/products";
 import axios from "axios";
+import Image from "next/image";
 
 const Sales: NextPageWithLayout = () => {
-  // axios get function to all sales "http://localhost:3001/sales"
-  // axios.get("http://localhost:3001/sales");
+  const [localSales, setLocalSales] = useState([]);
   const handleGetSales = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/sales");
-      console.log(response);
+      const { data } = await axios.get("http://localhost:3001/sales");
+      console.log(data);
+      setLocalSales(data);
     } catch (error) {
       console.log(error);
     }
@@ -19,7 +19,55 @@ const Sales: NextPageWithLayout = () => {
   useEffect(() => {
     handleGetSales();
   }, []);
-  return <div>Estas en sales</div>;
+
+  return (
+    <div className="px-16 py-10 mx-auto">
+      {localSales.map((sale: any, index) => (
+        <div
+          key={index}
+          className="tz-card tz-card-side bg-base-100 shadow-xl my-2 tz-card-bordered"
+        >
+          <figure
+            style={{
+              minHeight: "200px",
+              minWidth: "200px",
+              maxWidth: "200px",
+              maxHeight: "200px",
+            }}
+          >
+            <Image
+              width={200}
+              height={200}
+              src={sale.User.profileIMG}
+              alt="Movie"
+              style={{
+                minHeight: "150px",
+                minWidth: "150px",
+                maxWidth: "150px",
+                maxHeight: "150px",
+              }}
+            />
+          </figure>
+          <div className="tz-card-body">
+            <h2 className="tz-card-title">Usuario: {sale.User.name}</h2>
+            <h3 className="text-xl font-bold">Productos</h3>
+            {sale.Products.map((product: any) => (
+              <>
+                <div className="">
+                  <p className="font-bold">
+                    {product.name}{" "}
+                    <span className="tz-badge tz-badge-outline tz-badge-secondary">
+                      Precio: {product.price}
+                    </span>
+                  </p>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 Sales.getLayout = function getLayout(page: ReactElement) {
   return <AdminLayout>{page}</AdminLayout>;
